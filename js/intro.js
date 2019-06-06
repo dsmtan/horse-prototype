e264c9a776aad8e10b26f5b972e089d6f927b087
dsmtan <denisetan@live.nl>
2019-06-06 12:17:09 +0200

    i hate svgs

--- js/intro.js
+++ js/intro.js
@@ -1,37 +1,74 @@
 "use strict";
 
-//call openFullscreen after user has turned to landscape mode
-
-// function openFullscreen() {
-//   if (document.body.requestFullscreen) {
-//     document.body.requestFullscreen({ navigationUI: "hide" });
-//   } else if (document.body.mozRequestFullScreen) {
-//     /* Firefox */
-//     document.body.mozRequestFullScreen({ navigationUI: "hide" });
-//   } else if (document.body.webkitRequestFullscreen) {
-//     /* Chrome, Safari and Opera */
-//     document.body.webkitRequestFullscreen({ navigationUI: "hide" });
-//   } else if (document.body.msRequestFullscreen) {
-//     /* IE/Edge */
-//     document.body.msRequestFullscreen({ navigationUI: "hide" });
-//   }
-// }
-
-//svg for portrait to landscape animation
-// window.addEventListener("DOMContentLoaded", SVGloading);
-// function SVGloading() {
-//   fetch("/images/loading.svg")
-//     .then(response => response.text())
-//     .then(svgdata => {
-//       document
-//         .querySelector("#loading")
-//         .insertAdjacentHTML("afterbegin", svgdata);
-//     });
-// }
-
-window.addEventListener("DOMContentLoaded", loadIntroSVG);
+const introLayer = document.querySelector(".layer--intro");
+const instructLayer = document.querySelector(".layer--instructions");
+const gameName = document.querySelector(".h1--gamename");
 
 const firstPage = document.querySelector("#svg_firstpage");
+
+const playBtn = document.querySelector("#playBtn");
+const playBtn2 = document.querySelector("#playBtn2");
+
+// Find out if screen is in landscape mode
+let mql = window.matchMedia("(orientation: landscape)");
+const portraitLayer = document.querySelector(".layer--portrait");
+
+window.addEventListener("DOMContentLoaded", watchScreen);
+
+function watchScreen() {
+  // If it matches = landscape
+  if (mql.matches) {
+    // landscape orientation
+    portraitLayer.style.display = "none";
+    gameName.classList.add("animateName");
+    playBtn.classList.add("animateBtn");
+    loadIntroSVG();
+
+    openFullscreen();
+  } else {
+    // portrait orientation
+    portraitLayer.style.display = "block";
+    gameName.classList.remove("animateName");
+    playBtn.classList.remove("animateBtn");
+  }
+}
+
+mql.addListener(watchScreen);
+
+//call openFullscreen after user has turned to landscape mode
+
+function openFullscreen() {
+  // if (document.body.requestFullscreen) {
+  //   document.body.requestFullscreen({ navigationUI: "hide" });
+  // } else if (document.body.mozRequestFullScreen) {
+  //   /* Firefox */
+  //   document.body.mozRequestFullScreen({ navigationUI: "hide" });
+  // } else if (document.body.webkitRequestFullscreen) {
+  //   /* Chrome, Safari and Opera */
+  //   document.body.webkitRequestFullscreen({ navigationUI: "hide" });
+  // } else if (document.body.msRequestFullscreen) {
+  //   /* IE/Edge */
+  //   document.body.msRequestFullscreen({ navigationUI: "hide" });
+  // }
+}
+
+// window.onload = function() {
+//   portraitSVG();
+// };
+
+//svg for portrait to landscape animation
+window.addEventListener("DOMContentLoaded", portraitSVG);
+function portraitSVG() {
+  fetch("images/loading.svg")
+    .then(response => response.text())
+    .then(svgdata => {
+      document
+        .querySelector("#portrait")
+        .insertAdjacentHTML("afterbegin", svgdata);
+    });
+}
+
+// window.addEventListener("DOMContentLoaded", loadIntroSVG);
 
 //load introsvg for animations
 function loadIntroSVG() {
@@ -45,10 +82,6 @@ function loadIntroSVG() {
 }
 
 //when play button in intro is clicked layer disappears
-const playBtn = document.querySelector("#playBtn");
-const playBtn2 = document.querySelector("#playBtn2");
-const introLayer = document.querySelector(".layer--intro");
-const instructLayer = document.querySelector(".layer--instructions");
 
 playBtn.addEventListener("click", nextPage);
 playBtn2.addEventListener("click", nextPage);
